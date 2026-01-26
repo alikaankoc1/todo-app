c6750baa78a0924360a761d1ec27ccbaa5fe235b
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-26 15:26:12 +0300

    Implement Todo Removal and Prop Drilling Logic
    implement removeTodo logic in App.jsx
    
    connect removal event through TodoList
    
    wire up delete icon in Todo.jsx
    
    enhance component props and styling

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -3,9 +3,11 @@ import { IoIosRemoveCircle } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 import "../App.css";
 
-function Todo({ todo }) {
-  const { content } = todo;
-
+function Todo({ todo, onRemoveTodo }) {
+  const { id, content } = todo;
+  const removeTodo = () => {
+    onRemoveTodo(id);
+  };
   return (
     <div
       style={{
@@ -22,7 +24,7 @@ function Todo({ todo }) {
       <div>{content}</div>
 
       <div>
-        <IoIosRemoveCircle className="todo-icons" />
+        <IoIosRemoveCircle className="todo-icons" onClick={removeTodo} />
         <FaEdit className="todo-icons" />
       </div>
     </div>
