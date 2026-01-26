9b1e0b4f9d16787f8fcf5ca8bd624768015f8df8
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-26 16:17:40 +0300

    update App.css

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,31 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosRemoveCircle } from "react-icons/io";
 import { FaEdit } from "react-icons/fa";
 import "../App.css";
+import { FaCheck } from "react-icons/fa";
 
 function Todo({ todo, onRemoveTodo }) {
   const { id, content } = todo;
+  const [editable, setEdiTable] = useState(false);
   const removeTodo = () => {
     onRemoveTodo(id);
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        width: "100%",
-        border: "1px solid lightgray",
-        padding: "10px ",
-        marginTop: "12px",
-      }}
-    >
+    <div className="todo-item">
       <div>{content}</div>
-
       <div>
         <IoIosRemoveCircle className="todo-icons" onClick={removeTodo} />
-        <FaEdit className="todo-icons" />
+        {editable ? (
+          <FaCheck className="todo-icons" onClick={() => setEdiTable(false)} />
+        ) : (
+          <FaEdit className="todo-icons" onClick={() => setEdiTable(true)} />
+        )}
       </div>
     </div>
   );
